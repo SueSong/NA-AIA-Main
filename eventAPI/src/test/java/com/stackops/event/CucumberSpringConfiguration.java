a5635edffac0af008d061355353fe53ecd7b5018
SueSong <shufensong@hotmail.com>
2025-07-30 02:14:28 -0400

    Modify API TEST PORT

--- eventAPI/src/test/java/com/stackops/event/CucumberSpringConfiguration.java
+++ eventAPI/src/test/java/com/stackops/event/CucumberSpringConfiguration.java
@@ -6,8 +6,12 @@ import org.springframework.test.context.TestPropertySource;
 
 @CucumberContextConfiguration
 @SpringBootTest
+// @TestPropertySource(properties = {
+//     "event.api.baseUrl=http://localhost:8082/" // Set API base URL here
+// })
+
 @TestPropertySource(properties = {
-    "event.api.baseUrl=http://localhost:8082/" // Set API base URL here
+    "event.api.baseUrl=http://localhost:8080/" // Set API base URL here
 })
 public class CucumberSpringConfiguration {
     // No additional code is required here
